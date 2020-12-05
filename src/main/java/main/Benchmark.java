3602fe94de9f37cbfec9e9c348aa628829e96266
BraulioSanchez <braulio.sanchez@usp.br>
2020-12-05 14:10:39 -0300

    some changes

--- src/main/java/main/Benchmark.java
+++ src/main/java/main/Benchmark.java
@@ -12,7 +12,7 @@ public class Benchmark {
 	public static void main(String[] args) {
 		
 		ArrayList<Table> datasets = new ArrayList<>();
-//		datasets.add(Table.readCSV("datasets/parkinson.csv",   ",", 22, true));
+		datasets.add(Table.readCSV("datasets/parkinson.csv",   ",", 22, true));
 //		datasets.add(Table.readCSV("datasets/hepatitis.csv",   ",", 20, true));
 //		datasets.add(Table.readCSV("datasets/wine.csv",        ",", 13, true));
 //		datasets.add(Table.readCSV("datasets/glass.csv",       ",",  9, true));
@@ -23,24 +23,24 @@ public class Benchmark {
 //		datasets.add(Table.readCSV("datasets/pima.csv",        ",",  8, true));
 //		datasets.add(Table.readCSV("datasets/thyroid.csv",     ",",  6, true));
 //		datasets.add(Table.readCSV("datasets/satimage-2.csv",  ",", 36, true));
-		datasets.add(Table.readCSV("datasets/satellite.csv",   ",", 36, true));
+//		datasets.add(Table.readCSV("datasets/satellite.csv",   ",", 36, true));
 //		datasets.add(Table.readCSV("datasets/mammography.csv", ",",  6, true));
 //		datasets.add(Table.readCSV("datasets/shuttle.csv",     ",",  9, true));
 //		datasets.add(Table.readCSV("datasets/http.csv",        ",",  3, true));
 		
 		ArrayList<Quartet<String, Double, Integer, Integer>> parameters = new ArrayList<>();
-//		parameters.add(new Quartet<>("parkinson", 0.7482,    1,  10));
+		parameters.add(new Quartet<>("parkinson  ", 0.7482,    1,  30));
 //		parameters.add(new Quartet<>("hepatitis  ", 0.5190,    1,  10));
 //		parameters.add(new Quartet<>("wine       ", 0.5530,    27, 10));
 //		parameters.add(new Quartet<>("glass      ", 0.0758,    4,  10));
-//		parameters.add(new Quartet<>("vertebral  ", 0.4330,   82,  10));
+//		parameters.add(new Quartet<>("vertebral  ", 0.4711,   82,  100));
 //		parameters.add(new Quartet<>("ecoli      ", 0.1869,    7,  10));
 //		parameters.add(new Quartet<>("ionosphere ", 0.6266,    2,  90));
 //		parameters.add(new Quartet<>("breastw    ", 0.2346,    7,  10));
 //		parameters.add(new Quartet<>("pima       ", 0.5410,   54,  10));
 //		parameters.add(new Quartet<>("thyroid    ", 0.0001,    1,  10));
 //		parameters.add(new Quartet<>("satimage-2 ", 0.7657,   34, 100));
-		parameters.add(new Quartet<>("satellite  ", 0.2300,  110, 100));
+//		parameters.add(new Quartet<>("satellite  ", 0.2300,  110, 100));
 //		parameters.add(new Quartet<>("mammography", 0.0130,   65, 100));
 //		parameters.add(new Quartet<>("shuttle    ", 0.1266, 1230, 100));
 //		parameters.add(new Quartet<>("http       ", 0.2270, 1300, 100));
@@ -64,10 +64,12 @@ public class Benchmark {
 			int joinThreshold = parameters.get(i).getValue3();
 			
 			System.out.println("dataset,joinThs,outThs,range,auc,runtime");
-			for (double r = 0.01; r < 10.0; r = r + 0.01) {
-				for (int outThreshold = 101; outThreshold <= 150; outThreshold++) {
+			for (int outThreshold = 1; outThreshold <= 100; outThreshold++) {
+				double ratio = 1000.0;
+				for (int r = 1; r <= 10000; r++) {
+//				for (double r = .0001; r <= .1; r += .0001) {
 					long start = System.currentTimeMillis();
-					double[] output = SSJ(X, r, outThreshold, joinThreshold);
+					double[] output = SSJ(X, r/ratio, outThreshold, joinThreshold);
 					long end = System.currentTimeMillis();
 					double runtime = (end - start) / 1000.0;
 					double auc = AUC.measure(labels, output);
@@ -75,7 +77,7 @@ public class Benchmark {
 //					System.out.println(
 //							String.format("dataset=%s\tjoinThs=%d\toutThs=%d\trange=%.4f\tauc=%.4f\truntime=%.2f",
 //									dataset, joinThreshold, outThreshold, r, auc, runtime));
-					System.out.println(String.format("%s,%d,%d,%.4f,%.4f,%.3f", dataset, joinThreshold, outThreshold, r,
+					System.out.println(String.format("%s,%d,%d,%.4f,%.4f,%.3f", dataset, joinThreshold, outThreshold, r/ratio,
 							auc, runtime));
 				}
 			}
@@ -92,9 +94,8 @@ public class Benchmark {
 	}
 	
 	private static double[] SSJ(Table X, double radius, int threshold, int j) {
-		
 		ODSuperEGO superEgo = new ODSuperEGO(threshold, j);
-		superEgo.range(X, radius);	
+		superEgo.range(X, radius);
 		
 		double[] predict = new double[X.getNumRows()];
 		for (int i = 0; i < predict.length; i++) {
